a5ee1b904ca0037afb9ac60cd2e8ea6eacc87d33
Franks Vilca <franksjaviervilcaquispe@gmail.com>
2024-05-02 14:15:08 -0500

    Realizado el ejemplo de la profesora

--- Ejercicio_Docente/CajeraThread.java
+++ Ejercicio_Docente/CajeraThread.java
@@ -1,39 +1,66 @@
-public class Cajera implements Runnable {
+public class CajeraThread extends Thread {
     private String nombre;
     private Cliente cliente;
     private long initialTime;
 
-    // Constructor
-    public Cajera(String nombre, Cliente cliente, long initialTime) {
+    public CajeraThread() {
+    }
+
+    public CajeraThread(String nombre, Cliente cliente, long initialTime) {
         this.nombre = nombre;
         this.cliente = cliente;
         this.initialTime = initialTime;
     }
 
-    // Método run
-    @Override
-    public void run() {
-        System.out.println("La cajera " + this.nombre +
-                           " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
-                           + cliente.getNombre() + " EN EL TIEMPO: "
-                           + (System.currentTimeMillis() - this.initialTime) / 1000
-                           + "seg");
-
-        for (int i = 0; i < cliente.getCarroCompra().length; i++) {
-            this.esperarXsegundos(cliente.getCarroCompra()[i]);
-            System.out.println("Procesado el producto " + (i + 1)
-                               + " del cliente " + this.cliente.getNombre() +
-                               "->Tiempo: " + (System.currentTimeMillis() - this.initialTime) / 1000
-                               + "seg");
-        }
-
-        System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR "
-                           + cliente.getNombre() + " EN EL TIEMPO: "
-                           + (System.currentTimeMillis() - this.initialTime) / 1000
-                           + "seg");
+    public String getNombre() {
+        return nombre;
     }
 
-    // Método para simular el escaneo de productos
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getInitialTime() {
+        return initialTime;
+    }
+
+    public void setInitialTime(long initialTime) {
+        this.initialTime = initialTime;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("La cajera " + this.nombre + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
+
+                + this.cliente.getNombre() + " EN EL TIEMPO: "
+                + (System.currentTimeMillis() - this.initialTime) / 1000
+                + "seg");
+
+        for (int i = 0; i < this.cliente.getCarroCompra().length; i++) {
+            // Se procesa el pedido en X segundos
+            this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            System.out.println("Procesado el producto " + (i + 1)
+
+                    + " del cliente " + this.cliente.getNombre() + "->Tiempo: "
+                    + (System.currentTimeMillis() - this.initialTime) / 1000
+                    + "seg");
+
+        }
+        System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR "
+                + this.cliente.getNombre() + " EN EL TIEMPO: "
+                + (System.currentTimeMillis() - this.initialTime) / 1000
+                + "seg");
+
+    }
+
     private void esperarXsegundos(int segundos) {
         try {
             Thread.sleep(segundos * 1000);
